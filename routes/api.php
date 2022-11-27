cbc626076f3fdd0e0a976b680f0f656e5da27484
alaaahmeedd <alaa.aahmeedd.com>
2022-11-27 15:13:52 +0200

    added middleware

--- routes/api.php
+++ routes/api.php
@@ -22,18 +22,22 @@ use App\Http\Controllers\PatientController;
 Route::post('login', [StaffController::class, 'login']);
 Route::post('register', [StaffController::class, 'register']);
 
-Route::get('department', [DepartmentController::class, 'index']);
-Route::get('department/{id}', [DepartmentController::class, 'show']);
-Route::put('department/{id}', [DepartmentController::class, 'update']);
-Route::put('department/{id}', [DepartmentController::class, 'destroy']);
-Route::post('department', [DepartmentController::class, 'create']);
 
-Route::post('patient', [PatientController::class, 'store']);
-Route::get('patient', [PatientController::class, 'index']);
-Route::get('patient/{id}', [PatientController::class, 'show']);
-Route::put('patient/{id}', [PatientController::class, 'update']);
-Route::put('patient/{id}', [PatientController::class, 'destroy']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    Route::group(['middleware' => ['jwt.verify']], function(){
+        // Department
+        Route::get('department', [DepartmentController::class, 'index']);
+        Route::get('department/{id}', [DepartmentController::class, 'show']);
+        Route::put('department/{id}', [DepartmentController::class, 'update']);
+        Route::put('department/{id}', [DepartmentController::class, 'destroy']);
+        Route::post('department', [DepartmentController::class, 'create']);
+
+        // Patient
+        Route::post('patient', [PatientController::class, 'store']);
+        Route::get('patient/{id}', [PatientController::class, 'show']);
+        Route::put('patient/{id}', [PatientController::class, 'update']);
+        Route::put('patient/{id}', [PatientController::class, 'destroy']);
+        Route::get('patient', [PatientController::class, 'index']);
+    // return $request->user();
 });
